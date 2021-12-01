91a118cdf179d4b58a72040706eb74ea7addfd7c
SHYordanov20 <85336785+SHYordanov20@users.noreply.github.com>
2021-11-30 23:54:34 -1000

    Update calculator.cpp

--- Physics Calculator/calculator.cpp
+++ Physics Calculator/calculator.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     char op;
     float num1, num2;
-    cout << "Formulas" << endl;
+    cout << "**Formulas**" << endl;
     cout << "Average Speed Formula = 1 " << endl;
     cout << "Density Formula  = 2 " << endl;
     cout << "Power Formula = 3 " << endl;
@@ -17,8 +17,16 @@ int main() {
 
     switch(op) {
         case '1':
-            cout << num1 << "km" << " * " << num2 << "h"<< " = " << num1 / num2 << "km/h";
+            cout << num1 << "km" << " / " << num2 << "h"<< " = " << num1 / num2 << "km/h";
             break;
+
+       
+
+        case '3':
+            cout << num1 << "J" << " / " << num2 << "s" << " = " << num1 / num2 << "J/s";
+            break;
+
+       
 
         default:
             cout << "Error!";
