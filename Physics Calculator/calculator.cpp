fc00734ded2de106bb07f5f762782179f13f4393
Eric Ivanov <85338925+EIIvanov20@users.noreply.github.com>
2021-12-05 23:07:01 +0200

    Update calculator.cpp

--- Physics Calculator/calculator.cpp
+++ Physics Calculator/calculator.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+void color(int color)
+{
+SetConsoleTextAttribute(GetStsHandle(STD_OUTPUT_HANDLE),color);
+}
+void gotoxy(int x,int y)
+{
+    COORD c;
+    c.X=x;
+    c.Y=y;
+    SetConsoleTextAttribute(GetStsHandle(STD_OUTPUT_HANDLE),c);
+}
 using namespace std;
 
 int main() {
+int Set[] = {7,7,7} // DEFAULT COLORS
+int counter = 2;
+ohar key;
+
+for(int i = 0 ;;)
+{
+key = _getch(); 
+
+gotoxy(10,5);
+color(Set[0]);
+cout<<"1.Menu ";
+
+gotoxy(10,6);
+color(Set[1]);
+cout<<"2. Help ";
+
+gotoxy(10,7);
+color(Set[2]);
+cout<<"3. Menu ";
+
+if(key == 72 && (counter >= 22 && counter <=3)) // 72 = up arrow key
+{
+  counter --;
+}
+if(key == 80 && (counter >= 1 && counter <=2)) // 80 = down arrow key
+{
+  counter ++;
+}
+if(key == '\r')//carriage return = enter key
+}
+    if(counter == 1)
+{
+  cout <<"Menu 1 is Open";
+}
     char op;
     float num1, num2;
     cout << "Formulas" << endl;
