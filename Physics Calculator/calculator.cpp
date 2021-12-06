46992a65724176812fd12b541f1621c6a550a69d
Lubomir Georgiev <86322070+LRGeorgiev@users.noreply.github.com>
2021-12-06 12:48:44 +0200

    Complete Menu rework

--- Physics Calculator/calculator.cpp
+++ Physics Calculator/calculator.cpp
@@ -1,54 +1,49 @@
 #include <iostream>
-#include <windows.h>
-#include <conio.h>
+#include <iostream>
 
-void color(int color)
-{
-SetConsoleTextAttribute(GetStsHandle(STD_OUTPUT_HANDLE),color);
-}
-void gotoxy(int x,int y)
-{
-    COORD c;
-    c.X=x;
-    c.Y=y;
-    SetConsoleTextAttribute(GetStsHandle(STD_OUTPUT_HANDLE),c);
-}
 using namespace std;
 
-int main() {
-int Set[] = {7,7,7} // DEFAULT COLORS
-int counter = 2;
-ohar key;
-
-for(int i = 0 ;;)
+int main()
 {
-key = _getch(); 
+int choice;
+bool gameOn = true;
+while (gameOn != false){
+cout << "*******************************\n";
+cout << " 1 - Calculator.\n";
+cout << " 2 - Guide.\n";
+cout << " 3 - Our team.\n";
+cout << " 4 - Exit.\n";
+cout << " Enter your choice and press return: ";
 
-gotoxy(10,5);
-color(Set[0]);
-cout<<"1.Menu ";
+cin >> choice;
 
-gotoxy(10,6);
-color(Set[1]);
-cout<<"2. Help ";
-
-gotoxy(10,7);
-color(Set[2]);
-cout<<"3. Menu ";
-
-if(key == 72 && (counter >= 22 && counter <=3)) // 72 = up arrow key
+switch (choice)
 {
-  counter --;
+case 1:
+cout << "Calculator:\n";
+// rest of code here
+break;
+case 2:
+cout << "Guide\n";
+// rest of code here
+break;
+case 3:
+cout << "Our team\n";
+// rest of code here
+break;
+case 4:
+cout << "End of Program.\n";
+gameOn = false;
+break;
+default:
+cout << "Not a Valid Choice. \n";
+cout << "Choose again.\n";
+cin >> choice;
+break;
 }
-if(key == 80 && (counter >= 1 && counter <=2)) // 80 = down arrow key
-{
-  counter ++;
+
 }
-if(key == '\r')//carriage return = enter key
-}
-    if(counter == 1)
-{
-  cout <<"Menu 1 is Open";
+return 0;
 }
     char op;
     float num1, num2;
