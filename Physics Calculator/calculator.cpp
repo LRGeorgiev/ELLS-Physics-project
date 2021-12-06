e82c44b7c9e4e2c99a8734f5d7ef8914caee66b0
Lubomir Georgiev <86322070+LRGeorgiev@users.noreply.github.com>
2021-12-06 12:56:42 +0200

    Major updates
    Embedded-The calculator in the "Calculator" section, Instructions in the "Guide" sections, Info about the team in the "Our team" section,

--- Physics Calculator/calculator.cpp
+++ Physics Calculator/calculator.cpp
@@ -1,84 +1,85 @@
 #include <iostream>
-#include <iostream>
 
 using namespace std;
 
+
 int main()
 {
-int choice;
-bool gameOn = true;
-while (gameOn != false){
-cout << "*******************************\n";
-cout << " 1 - Calculator.\n";
-cout << " 2 - Guide.\n";
-cout << " 3 - Our team.\n";
-cout << " 4 - Exit.\n";
-cout << " Enter your choice and press return: ";
+	int choice;
+	bool calcOn = true;
+	char op;
+	float num1, num2;
+	while (calcOn != false) {
+		cout << "*******************************\n";
+		cout << " 1 - Calculator.\n";
+		cout << " 2 - Guide.\n";
+		cout << " 3 - Our Team.\n";
+		cout << " 4 - Exit.\n";
+		cout << " Enter your choice and press return: ";
 
-cin >> choice;
+		cin >> choice;
 
-switch (choice)
-{
-case 1:
-cout << "Calculator:\n";
-// rest of code here
-break;
-case 2:
-cout << "Guide\n";
-// rest of code here
-break;
-case 3:
-cout << "Our team\n";
-// rest of code here
-break;
-case 4:
-cout << "End of Program.\n";
-gameOn = false;
-break;
-default:
-cout << "Not a Valid Choice. \n";
-cout << "Choose again.\n";
-cin >> choice;
-break;
-}
+		switch (choice)
+		{
+		case 1:
+			cout << "Calculator:\n";
+			
+			cout << "Formulas" << endl;
+			cout << "Average Speed Formula = 1 " << endl;
+			cout << "Density Formula  = 2 " << endl;
+			cout << "Power Formula = 3 " << endl;
+			cout << "Newton’s Second Law = 4 " << endl;
+			cout << "Enter Formula: ";
+			cin >> op;
 
-}
-return 0;
-}
-    char op;
-    float num1, num2;
-    cout << "Formulas" << endl;
-    cout << "Average Speed Formula = 1 " << endl;
-    cout << "Density Formula  = 2 " << endl;
-    cout << "Power Formula = 3 " << endl;
-    cout << "Newton’s Second Law = 4 " << endl;
-    cout << "Enter Formula: ";
-    cin >> op;
+			cout << "Enter Two Numbers: ";
+			cin >> num1 >> num2;
 
-    cout << "Enter Two Numbers: ";
-    cin >> num1 >> num2;
+			switch (op) {
+			case '1':
+				cout << num1 << "km" << " / " << num2 << "h" << " = " << num1 / num2 << "km/h\n";
+				break;
 
-    switch(op) {
-        case '1':
-            cout << num1 << "km" << " / " << num2 << "h"<< " = " << num1 / num2 << "km/h";
-            break;
+			case '2':
+				cout << num1 << "kg" << " / " << num2 << "m" << " = " << num1 / num2 << "kg/m\n ";
+				break;
 
-        case '2':
-            cout << num1 << "kg" << " / "<< num2 << "m" <<" = " << num1/num2 << "kg/m ";
-            break;
+			case '3':
+				cout << num1 << "J" << " / " << num2 << "s" << " = " << num1 / num2 << "J/s\n";
+				break;
 
-        case '3':
-            cout << num1 << "J" << " / " << num2 << "s" << " = " << num1 / num2 << "J/s";
-            break;
+			case '4':
+				cout << num1 << "kg" << " * " << num2 << "m/s" << " = " << num1 * num2 << "N\n";
+				break;
 
-        case '4':
-            cout << num1 << "kg" << " * " << num2 << "m/s" << " = " << num1 * num2 << "N";
-            break;
+			default:
+				cout << "Error!\n";
+				break;
+			}
 
-        default:
-            cout << "Error!";
-            break;
-    }
+			break;
+		case 2:
+			cout << "To use the calculator just choose the formula you want and then type two numbers.\n";
+		
+			break;
+		case 3:
+			cout << "Our team:\n";
+			cout << "Lubomir Georgiev-Leader and Scrum trainer\n";
+			cout << "Eric Ivanov and Stanislav Yordanov-Back end Developers\n";
+			cout << "Lorena Hristova-QA engineer\n";
+			
+			break;
+		case 4:
+			cout << "End of Program.\n";
+			calcOn = false;
+			break;
+		default:
+			cout << "Not a Valid Choice. \n";
+			cout << "Choose again.\n";
+			cin >> choice;
+			break;
+		}
 
-    return 0;
+	}
+	return 0;
 }
